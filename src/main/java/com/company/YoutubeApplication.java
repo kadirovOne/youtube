5c9b1d1341e412cf8cabb1784d7e6b24d0a6e22c
Hudoberdi <hudoberdi555@gmail.com>
2022-07-06 22:32:52 +0500

    07.06.2022

---
+++ src/main/java/com/company/YoutubeApplication.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeApplication {
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoutubeApplication.class, args);
+    }
+
+}
